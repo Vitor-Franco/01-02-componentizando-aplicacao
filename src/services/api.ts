0ad9ea159926ffc8fb6f43a5fbd528f701217a5f
VitorFranco <vitoroliveirafranco@gmail.com>
2021-03-22 07:29:56 -0300

    Alterado BASE_URL da aplicação

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Vitor-Franco/01-02-componentizando-aplicacao/',
+  baseURL: 'http://localhost:3333',
 });
